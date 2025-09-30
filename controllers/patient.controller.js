6197e039d303af220dda11ae332b681b4797879b
AspiringSaint <wilberttosoydev@outlook.com>
2025-09-30 19:16:45 +0800

    added the get patient by id function

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -4,6 +4,7 @@ const Schedule = require('../models/Schedule');
 const mongoose = require('mongoose');
 const asyncHandler = require('express-async-handler');
 
+
 const createPatient = asyncHandler ( async (req, res) => {
     const { firstName, lastName, suffix, dob, gender, address, contactNumber } = req.body;
 
@@ -30,6 +31,7 @@ const createPatient = asyncHandler ( async (req, res) => {
     res.status(201).json({ message: 'New patient data created successfully' });
 });
 
+
 const getPatients = asyncHandler ( async (req, res) => {
     const patients = await Patient.find();
 
@@ -40,7 +42,24 @@ const getPatients = asyncHandler ( async (req, res) => {
     res.status(200).json(patients);
 });
 
+
+const getPatientById = asyncHandler ( async (req, res) => {
+    const { id } = req.params;
+
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid Patient Id' });
+    }
+
+    const patient = await Patient.findById(id);
+    if (!patient) {
+        return res.status(404).json({ message: 'Patient not found' });
+    };
+
+    res.status(200).json(patient);
+})
+
 module.exports = {
     createPatient,
-    getPatients
+    getPatients,
+    getPatientById
 };
--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const { authenticate, authorize } = require('../middlewares/auth.middleware');
 const {
     createPatient,
-    getPatients
+    getPatients,
+    getPatientById
 } = require('../controllers/patient.controller');
 
 router.post('/', createPatient);
 router.get('/', getPatients);
+router.get('/:id', getPatientById)
 
 module.exports = router;
